c7696c59321efe04f7036871ccd9d8cdb6dde627
Nathan Bonifacio <nathanpaivabonifacio@gmail.com>
2023-08-27 20:10:15 -0300

    projeto HTML e CSS com bootstrap sobre a Marvel

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+// const publicApiKey = 'b1e6ba8121b189a544f9ad2e48070806';
+// const timestamp = '1693148128';
+// const md5 = '9a3a6a602ae87006c2d431bfe825dc99';
+
+// //insert a slide for a hero
+// function setAllHeroes(heroes){
+
+// }
+
+// // // search all marvel's heroes
+// function getAllHeroes(){
+//     fatch(`http://gateway.marvel.com/v1/public/characters?ts=${timestamp}&apikey=${publicApiKey}&hash=${md5}`)
+//     .then((response) => {
+//         return response.json();
+//     }) .then((jsonParsed) => {
+//         console.log(jsonParsed);
+//     })
+// }
+
+// fatch(`http://gateway.marvel.com/v1/public/characters?ts=${timestamp}&apikey=${publicApiKey}&hash=${md5}`)
+// .then((response) => {
+//     return response.json();
+// }) .then((jsonParsed) => {
+//     console.log(jsonParsed);
+// })
